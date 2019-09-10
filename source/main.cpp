7e67fa4ed55cbcd1764cbab0bf9661b4419b8026
PYZ <592744797@qq.com>
2019-09-11 00:13:03 +0800

    [feature]:cmake添加glfw动态库

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+#include <GLFW/glfw3.h>
 int main()
 {
-	getchar();
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//	GLFWwindow* window = glfwCreateWindow(800, 600, "Star", NULL, NULL);
+//	getchar();
 	return 0;
 }
