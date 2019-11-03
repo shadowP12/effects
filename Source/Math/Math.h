13afc0267fad466b80242608f831bb9fa567ea2f
panyz <592744797@qq.com>
2019-11-03 22:21:10 +0800

    [feature]:新增三个工具函数

--- Source/Math/Math.h
+++ Source/Math/Math.h
@@ -4,3 +4,18 @@
 #include <gtc/type_ptr.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <gtx/quaternion.hpp>
+
+inline glm::vec3 getAxisX(const glm::mat4& mat)
+{
+	return glm::vec3(mat[0][0], mat[1][0], mat[2][0]);
+}
+
+inline glm::vec3 getAxisY(const glm::mat4& mat)
+{
+	return glm::vec3(mat[0][1], mat[1][1], mat[2][1]);
+}
+
+inline glm::vec3 getAxisZ(const glm::mat4& mat)
+{
+	return glm::vec3(mat[0][2], mat[1][2], mat[2][2]);
+}
